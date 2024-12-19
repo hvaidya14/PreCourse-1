1d0d96d5337175e5404ed54a59d387495c8d53fc
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2024-12-19 09:14:14 -0800

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,38 +1,52 @@
 class Stack { 
     //Please read sample.java file before starting.
-    //Kindly include Time and Space complexity at top of each file
+  //Kindly include Time and Space complexity at top of each file
     static final int MAX = 1000; 
     int top; 
     int a[] = new int[MAX]; // Maximum size of Stack 
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+        if (a[0] == 0) {
+            return true;
+        }
+        return false;
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        //Initialize your constructor
+        top=-1;
     } 
   
     boolean push(int x) 
-    { 
-        //Check for stack Overflow
-        //Write your code here
+    {
+        top++;
+        if (a[top] == MAX) {
+            System.out.println("Stack is full");
+            return false;
+        }
+        a[top] = x;
+        return true;
     } 
   
     int pop() 
-    { 
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+    {
+        if (top != -1) {
+            int data = a[top];
+            a[top] =0;
+            top--;
+            return data;
+        }
+        System.out.println("Stack is empty");
+        return 0;
     } 
   
     int peek() 
     { 
-        //Write your code here
+        return a[top];
     } 
-} 
-  
+}
 // Driver code 
 class Main { 
     public static void main(String args[]) 
